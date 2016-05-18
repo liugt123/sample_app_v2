40de35ef619acc863a03b70c7424429bcef02a96
Liu <514741077@qq.com>
2016-05-19 00:35:04 +0800

    finish 10.1.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
-	attr_accessor :remember_token
-	before_save { self.email = email.downcase }
+	attr_accessor :remember_token, :activation_token
+	before_save   :downcase_email
+	before_create :create_activation_digest
+	# before_save { self.email = email.downcase }
 	validates :name, presence: true, length: {maximum: 50}
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: {maximum: 255}, 
@@ -31,5 +33,17 @@ class User < ActiveRecord::Base
 	def forget
 		update_attribute(:remember_digest, nil)
 	end
+
+	private
+		#把电子邮件地址转换为小写
+		def downcase_email
+			self.email = email.downcase
+		end
+
+		#创建并赋值激活令牌和摘要
+		def create_activation_digest
+			self.activation_token = User.new_token
+			self.activation_digest = User.digest(activation_token)
+		end
 
 end
