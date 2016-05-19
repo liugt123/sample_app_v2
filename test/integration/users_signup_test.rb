db02a0bc504178a97f7f17d230e9fe4b281dccf8
Liu <514741077@qq.com>
2016-05-19 15:19:48 +0800

    finish 10.1

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,6 +4,11 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  
+  def setup
+    ActionMailer::Base.deliveries.claer
+  end
+
   test "invalid signup information" do
   	get signup_path
   	assert_no_difference 'User.count' do
@@ -13,23 +18,38 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   								 password_confirmation: "bar" }
   	end
   	assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
   end
 
-  test "valid signup information" do
+  test "valid signup information with account activation" do
   	get signup_path
-  	name       = "Example User"
-  	email      = "user@example.com"
-  	password   = "password"
   	assert_difference 'User.count', 1 do
-  		post_via_redirect users_path, user: {
-  			name: name,
-  			email: email,
-  			password: password,
-  			password_confirmation: password
+  		post users_path, user: {
+  			name: "Example User",
+  			email: "user@example.com",
+  			password: "password",
+  			password_confirmation: "password"
   		}
   	end
-  	# assert_template 'users/show'
-   #  assert is_logged_in?
+  	assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not_user.activated?
+    # 尝试在激活之前登陆
+    log_in_as(user)
+    assert_not is_logged_in?
+    # 激活令牌无效
+    get edit_account_activation_path("invalid token")
+    assert_not is_logged_in?
+    # 令牌有效，但电子邮箱不对
+    get edit_account_activation_path(user.activation_token, email: 'wrong')
+    assert_not is_logged_in?
+    # 激活令牌有效
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    asseert user,reload.activated?
+    follow_redirect!
+    assert_template 'users/show'
+    assert is_logged_in?
   end
 
   		
