0d57e1fd9b519b9b9811459ee3b5de0f0aa2a2e1
Liu <514741077@qq.com>
2016-05-20 01:16:17 +0800

    finish 10.2.4

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,8 +9,6 @@ class UserMailer < ApplicationMailer
   def account_activation(user)
     @user = user
     mail to: user.email, subject: "Account activation"
-
-    # mail to: "to@example.org"
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -18,9 +16,8 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.password_reset.subject
   #
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "Password reset"
   end
 end
